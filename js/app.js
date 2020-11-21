a8bb3dc232bd9f404e11f1b8446349e36fa8561b
Juan Laguna <juan_laguna_7@hotmail.com>
2020-11-20 22:05:07 -0300

    Primer commit del proyecto Cotizador de Criptomonedas

---
+++ js/app.js
@@ -0,0 +1,174 @@
+// SELECTORES
+
+const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+};
+
+// Crear un promise
+const obtenerCriptomonedas = criptoMonedas => new Promise(resolve => {
+    resolve(criptoMonedas);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario);
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
+
+});
+
+function consultarCriptomonedas() {
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => obtenerCriptomonedas(resultado.Data))
+        .then(criptomonedas => selectCriptomonedas(criptomonedas))
+
+        
+}
+
+// Llena el select
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach(cripto => {
+        const { FullName, Name } = cripto.CoinInfo;
+
+        const option = document.createElement('option');
+        option.value = Name;
+        option.textContent = FullName;
+
+        criptomonedasSelect.appendChild(option);
+      
+    });
+
+}
+
+function leerValor(e)  {
+    objBusqueda[e.target.name] = e.target.value;
+    // console.log(objBusqueda);
+}
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    // Validacion del formulario
+    const { moneda, criptomoneda } = objBusqueda;
+
+    if (moneda === '' || criptomoneda === '') {
+
+        mostrarAlerta('Ambos campos son obligatorios');
+
+        return;
+    }
+
+    // Consultar la API con los resultados
+    consultarApi();
+
+}
+
+function mostrarAlerta(mensaje) {
+
+    const existeError = document.querySelector('.error');
+
+    if (!existeError) {
+
+        const divMensaje = document.createElement('div');
+        
+        divMensaje.classList.add('error');
+
+        divMensaje.textContent = mensaje;
+
+        formulario.appendChild(divMensaje);
+
+        setTimeout(() => {
+        divMensaje.remove();
+        }, 3000);
+
+    }
+    
+    
+}
+
+function consultarApi() {
+    
+    const { moneda, criptomoneda } = objBusqueda;
+
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    mostrarSpinner();
+
+    fetch(url)
+        .then( respuesta => respuesta.json())
+        .then( cotizacion => {
+            mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda])  // Los valores criptomoneda y moneda deben ir en el mismo orden que en la url
+        });
+}
+
+function mostrarCotizacionHTML(cotizacion) {
+
+    limpiarHTML();
+
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+
+    const precio = document.createElement('p');
+    precio.classList.add('precio');
+    precio.innerHTML = `El precio es: <span>${PRICE}</span>`;
+
+    const precioAlto = document.createElement('p');
+    precioAlto.innerHTML = `Precio mas alto del dia <span>${HIGHDAY}</span>`;
+    
+    const precioBajo = document.createElement('p');
+    precioBajo.innerHTML = `Precio mas bajo del dia <span>${LOWDAY}</span>`;
+    
+    const ultimasHoras = document.createElement('p');
+    ultimasHoras.innerHTML = `Variacion ultimas 24 horas <span>${CHANGEPCT24HOUR} %</span>`;
+    
+    const ultimaActualizacion = document.createElement('p');
+    ultimaActualizacion.innerHTML = `Precio mas alto del dia <span>${LASTUPDATE}</span>`;
+
+
+
+
+
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(ultimasHoras);
+    resultado.appendChild(ultimaActualizacion);
+
+    formulario.reset();
+  
+}
+
+function limpiarHTML() {
+
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function mostrarSpinner() {
+
+    limpiarHTML();
+
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+
+    spinner.innerHTML = `
+    
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    
+    `;
+
+    resultado.appendChild(spinner);
+}
+
